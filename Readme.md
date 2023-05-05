c63d1a8ebc327f32894ffc68e9f8b3fa05257e33
beammnotfound <#include0613>
2023-05-06 04:56:45 +0800

    💼

---
+++ Readme.md
@@ -0,0 +1,37 @@
+<h1 align="center"> get-jobs-boss</h1>
+<div align="center">
+    💼自动在拉钩网上沟通以此来获得一份工作
+</div>
+
+## 如何使用
+由于使用selenium，所以
+### 第一步：下载ChromeDriver
+> https://sites.google.com/a/chromium.org/chromedriver/home
+
+### 第二步：配置ChromeDriver
+```java
+        System.setProperty("webdriver.chrome.driver", "/Users/beamstark/Desktop/chromedriver");
+```
+由于我是系统全局配置，所以这行代码没有在代码块中出现，需要的小伙伴自行添加代码配置
+
+### 第三步：修改代码
+打开 /src/main/java/ResumeSubmission.java <br>
+修改 **baseUrl** 为自己的筛选条件 <br>
+可以在 [拉钩网](https://www.lagou.com) 中筛选你的工作条件然后复制url ⚠️ 要为 **&pn=** 结尾<br>
+**page** ：从第几页开始投递，page不能小于1<br>
+**maxPage**：投递到第几页
+### 最后一步：运行代码
+需要扫码登陆
+<br>
+****
+
+**如果你想在 [Boss直聘](http://zhipin.com) 上自动投递简历，请 [Click me](https://github.com/BeammNotFound/get-jobs-boss).**
+<br>
+<br>
+
+> 希望能够在现在的大环境下帮助你找到一份满意的工作
+
+## 请我喝杯咖啡☕️
+<img src="./src/public/IMG_6480.JPG" alt="" width="300">
+
+<img src="./src/public/IMG_6479.JPG" alt="" width="300">
---
+++ src/main/java/ResumeSubmission.java
@@ -0,0 +1,74 @@
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+
+/**
+ * @author BeamStark
+ * 拉钩网自动投递简历
+ * @date 2023-05-04-15:21
+ */
+@Slf4j
+public class ResumeSubmission {
+    static Integer page = 1;
+    static Integer maxPage = 10;
+    static String baseUrl = "https://www.lagou.com/wn/jobs?px=new&pn=";
+    static String loginUrl = "https://www.lagou.com";
+    static ChromeDriver driver = new ChromeDriver();
+    static WebDriverWait wait10s = new WebDriverWait(driver, 10000);
+
+    public static void main(String[] args) {
+        login();
+        for (int i = page; i <= maxPage; i++) {
+            domain(i);
+        }
+    }
+
+    @SneakyThrows
+    private static void domain(int index) {
+        driver.get(baseUrl + index);
+        wait10s.until(ExpectedConditions.presenceOfElementLocated(By.id("openWinPostion")));
+
+        for (int i = 0; i < driver.findElements(By.id("openWinPostion")).size(); i++) {
+            newTab(i);
+        }
+    }
+
+    @SneakyThrows
+    private static void newTab(int index) {
+        String windowHandle = driver.getWindowHandle();
+        driver.findElements(By.id("openWinPostion")).get(index).click();
+        Set<String> windowHandles = driver.getWindowHandles();
+        windowHandles.remove(windowHandle);
+        String newWindowHandle = windowHandles.iterator().next();
+        driver.switchTo().window(newWindowHandle);
+        wait10s.until(ExpectedConditions.presenceOfElementLocated(By.className("resume-deliver")));
+        //点击投递简历
+        if (!"已投递".equals(driver.findElements(By.className("resume-deliver")).get(0).getText())) {
+            driver.findElements(By.className("resume-deliver")).get(0).click();
+            log.info("投递 {}", driver.findElement(By.className("position-head-wrap-name")).getText());
+        }
+        Thread.sleep(1000);
+        driver.close();
+        driver.switchTo().window(windowHandle);
+    }
+
+    @SneakyThrows
+    private static void login() {
+        driver.get(loginUrl);
+        wait10s.until(ExpectedConditions.presenceOfElementLocated(By.id("cboxClose")));
+        driver.findElement(By.id("cboxClose")).click();
+        driver.findElement(By.className("login")).click();
+        wait10s.until(ExpectedConditions.presenceOfElementLocated(By.className("text-tips-info")));
+        driver.findElements(By.className("text-tips-info")).get(1).click();
+        log.info("等待扫码..倒计时10s");
+        Thread.sleep(10000);
+    }
+}
